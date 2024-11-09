61573cdce34a40122ec300f38d92621c9f0a6293
정진원 <jinwonjung@jinwonjung.local>
2024-11-09 16:49:59 +0900

    feat: load tab, audio data in supabase storage

--- src/app/tab/[slug]/page.tsx
+++ src/app/tab/[slug]/page.tsx
@@ -1,22 +1,23 @@
+"use client";
+
 import React from "react";
 
 import Wrapper from "@/components/tab/composite/Wrappter";
+import useTab from "@/hooks/useTab/useTab";
 
-// type Props = {
-//   params: {
-//     slug: string;
-//   };
-// };
-
-const tabMockData = {
-  tabFileUrl: "/tab/green_day_basket_case.gp",
-  tabAudioUrl: "/musics/Basket_Case.mp3",
+type Props = {
+  params: {
+    slug: string;
+  };
 };
 
-export default function Page() {
+export default function Page(props: Props) {
+  const { params } = props;
+  const { slug } = params;
+  const tabData = useTab(slug);
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <Wrapper tabData={tabMockData} />
+      <Wrapper tabData={tabData} />
     </main>
   );
 }

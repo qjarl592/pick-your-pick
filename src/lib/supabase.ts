70babc227dd19593fe7df73b1d5e45f7c7a3e327
qjarl592 <81066891+qjarl592@users.noreply.github.com>
2024-11-23 19:35:17 +0900

    fix: delete newline char

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,7 +7,7 @@ const storageName = process.env.NEXT_PUBLIC_STORAGE_BUCKET as string;
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const getStorageUrl = async (path: string) => {
-  const { data } = await supabase.storage.from(storageName).getPublicUrl(path);
+  const { data } = await supabase.storage.from(storageName).getPublicUrl(path.trim());
   console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
   console.log(data);
   return data.publicUrl;

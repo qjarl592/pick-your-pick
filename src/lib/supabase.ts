2588488f77409211684c79cb210178b41c870cc1
정진원 <jinwonjung@jinwonjung.local>
2024-11-24 15:38:34 +0900

    fix: modify supabase url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = `https:/${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID?.trim()}.supabase.co`;
+const supabaseUrl = `https://${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID?.trim()}.supabase.co`;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const storageName = process.env.NEXT_PUBLIC_STORAGE_BUCKET as string;
 

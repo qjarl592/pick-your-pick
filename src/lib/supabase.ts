015be8548125bccefab4018eca6864f6d4910ee4
qjarl592 <81066891+qjarl592@users.noreply.github.com>
2024-11-24 15:17:39 +0900

    fix: delete space

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = `https:/${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID}.supabase.co`;
+const supabaseUrl = `https:/${process.env.NEXT_PUBLIC_SUPABASE_PROJECT_ID?.trim()}.supabase.co`;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 const storageName = process.env.NEXT_PUBLIC_STORAGE_BUCKET as string;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
-export const getStorageUrl = async (path: string) => {
-  const { data } = await supabase.storage.from(storageName).getPublicUrl(path.trim());
-  console.log(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string);
-  console.log(data);
+export const getStorageUrl = (path: string) => {
+  const { data } = supabase.storage.from(storageName).getPublicUrl(path);
   return data.publicUrl;
 };

ffac1203ab0c9783487eea0bf6022fd7f47406f1
정진원 <jinwonjung@jinwonjung.local>
2025-02-01 17:04:06 +0900

    feat: create SortSelector component

--- src/app/score/page.tsx
+++ src/app/score/page.tsx
@@ -1,9 +1,11 @@
 import SearchBar from "@/components/features/list/SearchBar";
+import SortSelector from "@/components/features/list/SortSelector";
 
 export default function Page() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <SearchBar />
+      <SortSelector />
     </main>
   );
 }

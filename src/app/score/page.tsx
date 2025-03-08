82b81f2aca1cf8df482b491eac70217109cdbd8b
qjarl592 <81066891+qjarl592@users.noreply.github.com>
2025-03-08 19:57:13 +0900

    feat: useQuery 조건부 랜더링 블록 조정

--- src/app/score/page.tsx
+++ src/app/score/page.tsx
@@ -3,24 +3,19 @@
 import { nanoid } from "nanoid";
 import { useSession } from "next-auth/react";
 
-import DifficultyFilter from "@/components/features/list/DifficultyFilter";
-import RecordingFilter from "@/components/features/list/RecordingFilter";
+import Logo from "@/components/common/Logo";
+import FilterWrap from "@/components/features/list/FilterWrap";
 import ScoreCard from "@/components/features/list/ScoreCard";
 import ScoreListCarousel from "@/components/features/list/ScoreListCarousel";
 import SearchBar from "@/components/features/list/SearchBar";
-import SortSelector from "@/components/features/list/SortSelector";
 import { useScores } from "@/hooks/useStores/useStores";
 
 export default function Page() {
   const { data: session, status: sessionStatus } = useSession();
-  const { data: scores, isLoading, error } = useScores(session?.user?.id || "");
+  const { data: scores, isLoading } = useScores(session?.user?.id || "");
 
-  if (sessionStatus === "loading" || isLoading) {
+  if (sessionStatus === "loading") {
     return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error loading scores</div>;
   }
 
   if (!session) {
@@ -28,14 +23,22 @@ export default function Page() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <SearchBar />
-      <SortSelector />
-      <DifficultyFilter />
-      <RecordingFilter />
-      <ScoreListCarousel>
-        {scores?.data?.map((score) => <ScoreCard key={nanoid()} score={score} />)}
-      </ScoreListCarousel>
+    <main className="flex min-h-screen flex-col items-center justify-start">
+      <div className="flex w-full flex-col items-center gap-6 p-20">
+        <Logo size="lg" />
+
+        <SearchBar />
+
+        <FilterWrap />
+
+        {scores ? (
+          <ScoreListCarousel>
+            {scores.data?.map((score) => <ScoreCard key={nanoid()} score={score} />)}
+          </ScoreListCarousel>
+        ) : (
+          <div>{isLoading ? "loading..." : "error"}</div>
+        )}
+      </div>
     </main>
   );
 }
